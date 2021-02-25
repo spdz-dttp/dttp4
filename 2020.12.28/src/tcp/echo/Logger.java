4ad72e0b4a6278912df629aef818e38d766669f1
带头逃跑 <2455050075@qq.com>
2021-02-25 13:57:45 +0800

    Tcp

---
+++ 2020.12.28/src/tcp/echo/Logger.java
@@ -0,0 +1,23 @@
+package tcp.echo;
+
+import java.util.Date;
+
+/**
+ * @program: 2020.12.28
+ * @description: ${description}
+ * @author: spdz
+ * @create: 2020-12-30 16:12
+ **/
+
+/**
+ * 把打印日志的代码封装一下
+ */
+public class Logger {
+    public static void debug(String message) {
+        System.out.printf("%s: %s: DEBUG: %s%n", Thread.currentThread().getName(), new Date(), message);
+    }
+
+    public static void error(String message) {
+        System.out.printf("%s: %s: ERROR: %s%n", Thread.currentThread().getName(), new Date(), message);
+    }
+}

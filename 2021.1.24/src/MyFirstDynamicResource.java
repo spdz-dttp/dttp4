fcdfc34403605d4260870beed7c4b9daeb56bc3e
带头逃跑 <2455050075@qq.com>
2021-03-05 12:01:36 +0800

    动态资源练习一

---
+++ 2021.1.24/src/MyFirstDynamicResource.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @program: 2021.1.24
+ * @description: 动态资源练习一
+ * @author: spdz
+ * @create: 2021-01-24 16:49
+ **/
+
+public class MyFirstDynamicResource extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 通过 query string，取到 wd 这个 name 所对应的 value
+        String wd = req.getParameter("wd");
+
+        // 2. 返回的 Content-Type 是 text/html; 字符集编码是 utf-8
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+
+        // 3. 输出的响应的内容
+        writer.println("<hl>我是 /s 资源</hl>");
+        writer.println("<p>用户要查找的关键字是：" + wd + "</p>");
+    }
+}
